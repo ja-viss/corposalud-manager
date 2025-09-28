8a51ad7e1ac27a26d02e284a0c4a92d862435275
Javier Sayago <javier.sayago000@gmail.com>
2025-09-28 15:16:22 +0000

    no uses el nombre de corpotachira, solo deja la imagen como logo complet

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,17 +2,14 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2.5 text-primary font-semibold tracking-tighter">
+    <div className="flex items-center">
        <Image
         src="/image.png"
-        alt="CORPOTACHIRA MANAGER Logo"
+        alt="Logo"
         width={32}
         height={32}
         className="rounded-lg"
       />
-      <span className="text-base group-data-[collapsible=icon]:hidden">
-        CORPOTACHIRA MANAGER
-      </span>
     </div>
   );
 }
