777b82d2ad13bd89fa5672679405241c77724fb4
Javier Sayago <javier.sayago000@gmail.com>
2025-09-28 12:36:30 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { cookies } from 'next/headers';
 
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
